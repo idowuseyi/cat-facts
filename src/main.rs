c17677ba8e3f2cf45fb5cbfd97eb5c6a32c0dfdf
idowuseyi <idowuseyi22@gmail.com>
2025-10-18 00:33:07 +0100

    add root route

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
         App::new()
             .route("/me", web::get().to(cat_fact_handler))
+            .route("/", web::get().to(|| async { "Hello Rust/Actix server responding!" }))
     )
     .bind("127.0.0.1:8080")?
     .run()
